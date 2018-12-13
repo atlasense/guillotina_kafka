2793960030919a1227fbdf1458a30d826fc3413f
Andy Leeb <andrew@onna.com>
2018-12-13 18:02:17 -0500

    remove all topics as part of teardown

--- guillotina_kafka/tests/fixtures.py
+++ guillotina_kafka/tests/fixtures.py
@@ -29,3 +29,18 @@ def kafka_container(kafka):
         "port": kafka[1],
     })
     yield kafka
+    import subprocess
+    from subprocess import check_output
+    cmd_path = '/opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh'
+    image_id = check_output(["docker ps | grep spotify | awk '{print $1}'"], shell=True)  # noqa
+    if image_id:
+        image_id = image_id.decode('utf-8').splitlines()[-1]
+        topics_list_cmd = f"docker exec -it {image_id} {cmd_path} --zookeeper 127.0.0.1:2181 --list"  # noqa
+        topics_list = check_output([topics_list_cmd], shell=True)
+        topics_list = topics_list.decode('utf-8')
+        for topic in topics_list:
+            topic = topic.decode('utf-8').splitlines()[-1]
+            if topic != "__consumer_offsets":
+                subprocess.Popen(
+                    [f"docker exec -it {image_id} {cmd_path} --zookeeper 127.0.0.1:2181 --delete --topic {topic}"],  # noqa
+                    shell=True)
